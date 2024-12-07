788611f668c08d1ee7e57960792c612d247e1e60
zarapar64 <zarapar64@gmail.com>
2024-12-07 19:23:54 +0100

    faq  ok

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -6,16 +6,16 @@ const AccordionItem = ({ item }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleAccordion = () => {
-    setIsOpen(!isOpen); 
+    setIsOpen(!isOpen);
   };
 
   return (
     <div className="faq">
-      <div className={`panel ${isOpen ? 'active' : ''}`} onClick={toggleAccordion}>
-        <span>{item.title}</span>
-        {/* <img src={chevronUpIcon} alt="Chevron icon" className="icon-upp" /> */}
-        {/* <i className="item-icon fa-solid fa-chevron-down"></i> */}
-        <i className={`item-icon fa-solid ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+      <div className="panel" onClick={toggleAccordion}>
+        <span style={{ textDecoration: isOpen ? 'underline' : 'none', fontWeight: 'bold' }}>
+          {item.title}
+        </span>
+        <img src={isOpen ? chevronUpIcon : chevronIcon} alt="Chevron icon" className="icon-upp" />
       </div>
 
       {isOpen && (
@@ -28,3 +28,39 @@ const AccordionItem = ({ item }) => {
 };
 
 export default AccordionItem;
+
+
+
+
+
+// import React, { useState } from 'react';
+// import chevronUpIcon from '../assets/images/Group 6.svg';
+// import chevronIcon from '../assets/images/Group 5.svg';
+
+// const AccordionItem = ({ item }) => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleAccordion = () => {
+//     setIsOpen(!isOpen); 
+//   };
+
+//   return (
+//     <div className="faq">
+//       <div className={`panel ${isOpen ? 'active' : ''}`} onClick={toggleAccordion}>
+//         {/* <span >{item.title}</span> */}
+//         <span style={{ textDecoration: 'none', fontWeight: 'bold' }}></span>
+//         {/* <img src={chevronUpIcon} alt="Chevron icon" className="icon-upp" /> */}
+//         {/* <i className="item-icon fa-solid fa-chevron-down"></i> */}
+//         <i className={` btn-circlee item-icon fa-solid ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+//       </div>
+
+//       {isOpen && (
+//         <div className="faq-text">
+//           <p>{item.content}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AccordionItem;
